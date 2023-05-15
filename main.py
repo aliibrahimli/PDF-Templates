def813cb24fe9cfdad6616f150ecd953c25c2b4a
ali ibrahimli <alibrahimli041@gmail.com>
2023-05-15 11:28:49 +0400

    Add lines

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
 
+    for i in range(20, 298, 10):
+        pdf.line(10, i, 200, i)
+
     pdf.line(10, 21, 200, 21)
 
     # Set the footer
@@ -28,6 +31,7 @@ for index, row in df.iterrows():
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
-
+        for i in range(20, 298, 10):
+            pdf.line(10, i, 200, i)
 
 pdf.output("output.pdf")
